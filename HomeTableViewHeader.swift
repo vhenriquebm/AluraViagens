aceae2f77095a4867a4b41c1987ad206c50994c7
Vitor Henrique <vhenriquebm@gmail.com>
2022-01-27 23:46:25 -0300

    Primeira versão do projeto - TableView dinamica criada, Arquivo Xib para o Header criado com a classe parametrizando sua altura

---
+++ HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  aluraViagens
+//
+//  Created by Vitor Henrique Barreiro Marinho on 27/01/22.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- aluraViagens/ViewController.swift
+++ aluraViagens/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viagensTableView.dataSource = self
+        viagensTableView.delegate = self
+    }
+    
+    @IBOutlet weak var viagensTableView: UITableView!
+}
+
+
+
+    
+    extension ViewController: UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+    
+        cell.textLabel?.text = "Viagem \(indexPath.row)"
+        
+        return cell
+    }
+    
     }
 
 
+
+extension ViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView = Bundle.main.loadNibNamed("HomeTableViewHeader", owner: self)?.first as? HomeTableViewHeader
+        
+        return headerView
+        
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 300
+    }
+    
+    
+    
 }
+    
+
+
+    
+    
+    
+
 
